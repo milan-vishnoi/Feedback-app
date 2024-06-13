2616b776ae6a1cbfc904503d8278ed46ee6f2f5c
Milan Vishnoi <milan.vish10@gmail.com>
2024-06-14 02:32:20 +0530

    Alternate Way to create HTML Element in JSX
    We can also use React.createElement in order to create the HTML elements in JSX by using JavaScript but that would not be easier to write. In JSX we need to use className instead of class and htmlFor instead of for

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import React from "react";
+
 function App() 
 {
     return (
-        <>
-            <h1>Hello from the React Functional Component</h1>
-            <p>This is a functional component and I am using paragraph</p>
-        </>
+React.createElement("div",{className:"container"},React.createElement("h1",{},"My App"))
 );
 }
 
