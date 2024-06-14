f9b227d1af0bbd28b9efa76d5ea3557e751d3be9
Milan Vishnoi <milan.vish10@gmail.com>
2024-06-14 17:40:06 +0530

    Imported and added the Header component into the App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import Header from "./components/Header";
+
 function App() 
 {
     return (
+      <>
+          <Header/>
           <div className="container">
-            <h1>Hello from the React Functional Component</h1>
-            <p>This is a functional component and I am using paragraph</p>
+            <h1>My App</h1>
           </div>
+      </>
+
 );
 }
 
