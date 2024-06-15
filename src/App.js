dd3a0ede99a135e64ca5e4822ced28b828a1d6a2
Milan Vishnoi <milan.vish10@gmail.com>
2024-06-15 17:24:33 +0530

    Added sample feedback data and App level state
    The previous commit had component level state while in thsi commit we defined the App level state.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import FeedbackItem from "./components/FeedbackItem.jsx";
+import FeedbackData from "./data/FeedbackData";
 
 function App() 
 {
+   const [feedback, setFeedback] = useState(FeedbackData)
     return (
       <>
           <Header text="Welcome to Feedback UI"/>
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id:1,
+        rating: 7,
+        text: "This was a decent app and easy to use"
+    },
+    {
+        id: 2,
+        rating: 8,
+        text:"Super impressed with the app"
+    },
+    {
+        id:3,
+        rating: 5,
+        text:"Wasn't too impressed with the App. It is okayish"
+    }
+];
+
+export default FeedbackData;
