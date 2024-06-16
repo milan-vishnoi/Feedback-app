8d56d7528d0236bbf0f0c37e19c8229e1bcf995f
Milan Vishnoi <milan.vish10@gmail.com>
2024-06-16 16:35:37 +0530

    Added propTypes for FeedbackItem and FeedbackList

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,12 +1,17 @@
+import PropTypes from 'prop-types'
 import Card from "./shared/Card";
 
 function FeedbackItem(props) {
   return (
-    <Card reverse={true}>
+    <Card>
         <div className="num-display">{props.item.rating}</div>
         <div className="text-display">{props.item.text}</div>
     </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired
 }
 
 export default FeedbackItem;
