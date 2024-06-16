f1d64c56af722b524fb989791178abcb05e27b78
Milan Vishnoi <milan.vish10@gmail.com>
2024-06-16 16:46:32 +0530

    Imported Font awesome into FeedbackItem
    We can set the color of the icon also by specifying the color in the props

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,14 @@
-import PropTypes from 'prop-types'
+import {FaTimes} from "react-icons/fa";
+import PropTypes from 'prop-types';
 import Card from "./shared/Card";
 
 function FeedbackItem(props) {
   return (
     <Card>
         <div className="num-display">{props.item.rating}</div>
+        <button className="close">
+          <FaTimes color="purple"/>
+        </button>
         <div className="text-display">{props.item.text}</div>
     </Card>
   )
