50dd8423d5d212f8765bf099d84d9df8216ddf03
Milan Vishnoi <milan.vish10@gmail.com>
2024-06-19 02:20:11 +0530

    Added details in AboutPage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,18 @@
+import Card from "../components/shared/Card";
+
 function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <Card>
+       <div className="about">
+        <h1>About This Project</h1>
+        <p>This the About page for this project of feedback app in React </p>
+         <p>Versio: 1.0.0</p>
+         <p>
+            <a href="/">Back to homepage</a>
+         </p>
+       </div>
+    </Card>
   )
 }
 
-export default AboutPage
+export default AboutPage;
