31a517553d40f87fa22e6878ee15f2715d147182
Milan Vishnoi <milan.vish10@gmail.com>
2024-06-19 02:16:15 +0530

    Added router to the App
    We use router to route the request as per url to the component. There are 2 types of it: BrowserRoute and HashRoute. In this case we have used BrowserRoute (and hence we don't need to use # in url). The Route should be wrapped within <Routes> and we mention the path as well as the element in the Route. The entire App JSX needs to be warpped inside <BroswerRouter> (or its alias name as we have done <Router>). We used exact in first case so that when /about is used the / component don't load.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
 import { useState } from "react";
-import {v4 as uid} from "uuid";
+import { v4 as uid } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList.jsx";
 import FeedbackStats from "./components/FeedbackStats.jsx";
 import FeedbackForm from "./components/FeedbackForm.jsx";
-import AboutPages from "./pages/AboutPage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
 import FeedbackData from "./data/FeedbackData";
 
-function App() 
-{
-   const [feedback, setFeedback] = useState(FeedbackData);
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
 
-   const addFeedback = (newFeedback) => {
-     newFeedback.id = uid();
-     setFeedback([newFeedback,...feedback]);
-   };
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uid();
+    setFeedback([newFeedback, ...feedback]);
+  };
 
-   const deleteFeedback = (id) => {
-      if(window.confirm("Are you sure you want to delete this feeback?")){
-        setFeedback(feedback.filter((item)=> item.id!== id));
-      }
-   }; 
-    return (
-      <>
-          <Header text="Welcome to Feedback App"/> 
-          <div className="container">
-          <FeedbackForm handleAdd={addFeedback}/>
-          <FeedbackStats feedback={feedback}/>
-            <FeedbackList feedback={feedback} handleDelete = {deleteFeedback}/>
-            <AboutPages/>
-          </div>
-      </>
-);
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete this feeback?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+  return (
+    <Router>
+      <Header text="Welcome to Feedback App" />
+      <div className="container">
+      <Routes>
+          <Route exact
+            path="/" 
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+              </>
+            } 
+          />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
