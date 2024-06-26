d214bff636b2dfa27881696a52b09c33ec86b9d7
Milan Vishnoi <milan.vish10@gmail.com>
2024-06-27 01:40:21 +0530

    Navigate component in react-router-dom
    We can redirect the request coming to a route to another route based on condition by returing the Navigate component. We need to specify which component we need to Navigate to. In this case if the status is 404, the request coming to /post will be navigated/redirected to /notfound. In react-router-dom 5 it used to "Redirect" component.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,19 @@
-import { useParams } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 function Post() {
-    const params = useParams();
+
+  const status = 404;
+
+  if(status === 404)
+    {
+      return <Navigate to="/notfound" />;
+    }
+
   return (
     <div>
-        <h1>Post {params.id}</h1>
-        <p>Name: {params.name}</p>
+        <h1>Post </h1>
     </div>
   )
 }
 
-export default Post
+export default Post;
